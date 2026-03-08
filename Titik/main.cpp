11aaf757afce2812ca701a88159e21d8da24e8e1
misbachulmunir-22 <almunirmisbah22@gmail.com>
2026-03-08 15:49:23 +0700

    Laporan Praktikum Pertemuan Pertama

---
+++ Titik/main.cpp
@@ -0,0 +1,30 @@
+#include <GL/glut.h>
+
+void point(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glPointSize(5.0f);
+    glBegin(GL_POINTS);
+
+    glColor3f(1.0f, 0.0f, 0.0f);        // warna merah
+    glVertex3f(0.25f, 0.25f, 0.0f);     // posisi titik
+
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(640, 480);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+
+    glutCreateWindow("MisbachulMunir(Titik)");
+    glutDisplayFunc(point);
+
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);   // warna background biru
+
+    glutMainLoop();
+    return 0;
+}
